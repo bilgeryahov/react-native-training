22a5078da25c767257f7a0cd9446261885a8f1c6
Bilger Yahov <bilger@studyportals.com>
2017-10-26 18:35:33 +0200

    Initial implementation of Header component. Add initial stylings.

--- MyApp/components/UserDetails/Header.js
+++ MyApp/components/UserDetails/Header.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { View, Text, Image } from 'react-native';
 
-const Header = () => {
+import styles from './styles';
 
-	return null;
+import { capitalizeFirstLetter } from '../../helpers/string';
+
+const Header = ({ picture, name }) => {
+
+	return (
+		<View>
+			<Image
+				source={{uri: picture.large}}
+			    style={styles.image}
+			/>
+			<Text>
+				{capitalizeFirstLetter(name.first)} {capitalizeFirstLetter(name.last)}
+			</Text>
+		</View>
+	);
 };
 
 export default Header;
--- MyApp/components/UserDetails/styles.js
+++ MyApp/components/UserDetails/styles.js
@@ -1,6 +1,12 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 import colors from '../../config/colors';
 
-export default StyleSheet.create({
+const window = Dimensions.get('window');
 
+export default StyleSheet.create({
+	image:{
+		width: window.width / 2,
+		height: window.width / 2,
+		borderRadius: window.width / 4
+	}
 });
