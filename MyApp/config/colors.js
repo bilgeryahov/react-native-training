918c82c34e62397ff0e88237703a30332cb1dd53
Bilger Yahov <bilger@studyportals.com>
2017-10-26 18:58:24 +0200

    Add new background color.

--- MyApp/config/colors.js
+++ MyApp/config/colors.js
@@ -3,5 +3,6 @@ export default {
 	primaryText: '#525252',
 	subtleText: '#9a9a9a',
 	rowUnderlay: 'rgba(154, 154, 154, 0.25)',
-	border: '#cccccc'
+	border: '#cccccc',
+	grayBackground: '#f9fafb'
 };
