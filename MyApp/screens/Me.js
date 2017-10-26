5a0cbab12d030a4b62321d5bdad0256ce626e40d
Bilger Yahov <bilger@studyportals.com>
2017-10-26 22:03:26 +0200

    Add a new Buttons component folder. Introduce PrimaryButton component.

--- MyApp/screens/Me.js
+++ MyApp/screens/Me.js
@@ -2,14 +2,20 @@ import React  from 'react';
 import { View, Text, ScrollView } from 'react-native';
 
 import { Header, Actions, Info } from '../components/UserDetails';
+import { PrimaryButton } from '../components/Buttons';
 import colors from '../config/colors';
 import { me } from '../config/data';
+
 
 class Me extends React.Component {
 	render() {
 		return (
 			<ScrollView style={{ backgroundColor: colors.background }}>
 				<Header {...me}/>
+				<PrimaryButton
+					label="Edit Profile"
+					onPress={() => null}
+				/>
 				<Actions {...me}/>
 				<Info {...me}/>
 			</ScrollView>
