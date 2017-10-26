afe5bb3bc234fc03dc8a7efaf6718f6bb81c5433
Bilger Yahov <bilger@studyportals.com>
2017-10-26 14:48:00 +0200

    Add new components as screens.

---
+++ MyApp/screens/Contacts.js
@@ -0,0 +1,13 @@
+import React  from 'react';
+
+class Contacts extends React.Component {
+	render() {
+		return (
+			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+				<Text>Contacts Screen</Text>
+			</View>
+		);
+	}
+}
+
+export default Contacts;
---
+++ MyApp/screens/Me.js
@@ -0,0 +1,13 @@
+import React  from 'react';
+
+class Me extends React.Component {
+	render() {
+		return (
+			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+				<Text>Me Screen</Text>
+			</View>
+		);
+	}
+}
+
+export default Me;
