0e0a97c1a69f2934fe82d917988edba386136203
Bilger Yahov <bilger@studyportals.com>
2017-10-26 21:18:16 +0200

    Add a TabNavigator.

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import { ContactsStack } from './config/router';
+import { Tabs } from './config/router';
 
 export default class App extends React.Component {
   render() {
     return (
-     <ContactsStack/>
+     <Tabs/>
     );
   }
 }
--- MyApp/config/router.js
+++ MyApp/config/router.js
@@ -1,7 +1,12 @@
-import { StackNavigator } from 'react-navigation';
+import React from 'react';
+import { StackNavigator, TabNavigator } from 'react-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 import Contacts from '../screens/Contacts';
 import Details from '../screens/Details';
+import NewContact from '../screens/NewContact';
+import Me from '../screens/Me';
 
 import { capitalizeFirstLetter } from '../helpers/string';
 
@@ -17,5 +22,29 @@ export const ContactsStack = StackNavigator({
 		navigationOptions: ({ navigation }) => ({
 			title: `${capitalizeFirstLetter(navigation.state.params.name.first)} ${capitalizeFirstLetter(navigation.state.params.name.last)}`
 		})
+	}
+});
+
+export const Tabs = TabNavigator({
+	Contacts: {
+		screen: ContactsStack,
+		navigationOptions: {
+			tabBarLabel: 'Contacts',
+			tabBarIcon: ({ tintColor }) => <Icon name="ios-list" size={35} color={tintColor}/>
+		}
+	},
+	NewContact: {
+		screen: NewContact,
+		navigationOptions: {
+			tabBarLabel: 'New Contact',
+			tabBarIcon: ({ tintColor }) => <Icon name="ios-add" size={35} color={tintColor}/>
+		}
+	},
+	Me: {
+		screen: Me,
+		navigationOptions: {
+			tabBarLabel: 'Me',
+			tabBarIcon: ({ tintColor }) => <Icon name="ios-contact" size={35} color={tintColor}/>
+		}
 	}
 });
