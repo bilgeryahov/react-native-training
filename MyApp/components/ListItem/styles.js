726102c9fc7ddc568a020028bf9cfccee76f0ce6
Bilger Yahov <bilger@studyportals.com>
2017-10-26 16:00:37 +0200

    Add stylings for ListItem component.

---
+++ MyApp/components/ListItem/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+	avatar:{
+		width: 40,
+		height: 40,
+		borderRadius: 20,
+		marginRight: 10
+	},
+	row:{
+		flexDirection: 'row',
+		alignItems: 'center',
+		paddingHorizontal: 15,
+		paddingVertical: 8
+	}
+});
