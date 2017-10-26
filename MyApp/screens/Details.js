cc6568939e0283ea997c96bb0296c3645bed1641
Bilger Yahov <bilger@studyportals.com>
2017-10-26 18:35:03 +0200

    Start using the Header component.

--- MyApp/screens/Details.js
+++ MyApp/screens/Details.js
@@ -1,11 +1,16 @@
 import React  from 'react';
-import { FlatList, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
+
+import { Header } from '../components/UserDetails';
+import colors from '../config/colors';
 
 class Details extends React.Component {
 	render() {
+		const contact = this.props.navigation.state.params;
+
 		return (
-			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-				<Text>Details Screen</Text>
+			<View style={{ backgroundColor: colors.background }}>
+				<Header {...contact} />
 			</View>
 		);
 	}
