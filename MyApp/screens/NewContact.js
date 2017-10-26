69f1b8bfa30ae1df14cf2efbde74d36df78fc927
Bilger Yahov <bilger@studyportals.com>
2017-10-26 23:07:29 +0200

    Implement the NewContact screen.

--- MyApp/screens/NewContact.js
+++ MyApp/screens/NewContact.js
@@ -1,21 +1,61 @@
 import React  from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, View } from 'react-native';
+
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scrollview';
 
 import { CustomTextInput } from '../components/CustomTextInput';
+import { PrimaryButton } from '../components/Buttons';
 
 import colors from '../config/colors';
 
+const fields = [
+	{ placeholder: 'First Name...', stateKey: 'firstName' },
+	{ placeholder: 'Last Name...', stateKey: 'lastName' },
+	{ placeholder: 'Email...', stateKey: 'email', keyboardType: 'email-address' },
+	{ placeholder: 'Mobile Phone...', stateKey: 'mobilePhone' },
+	{ placeholder: 'Home Phone...', stateKey: 'homePhone' },
+	{ placeholder: 'City...', stateKey: 'City' },
+	{ placeholder: 'Birthday...', stateKey: 'birthday' },
+	{ placeholder: 'Registered...', stateKey: 'registered' },
+	{ placeholder: 'User Name...', stateKey: 'username' }
+];
+
 class NewContact extends React.Component {
+	constructor(props){
+		super(props);
+
+		this.state = {};
+	}
+
+	onInputChange = (text, stateKey) => {
+		const mod = {};
+		mod[stateKey] = text;
+		this.setState(mod);
+	};
+
+	handleSubmit = () => {
+		alert('Submitted!');
+	};
+
 	render() {
 		return (
-			<ScrollView style={{ backgroundColor: colors.background }}>
-				<CustomTextInput
-					placeholder="First Name..."
-				/>
-				<CustomTextInput
-					placeholder="Last Name..."
-				/>
-			</ScrollView>
+			<KeyboardAwareScrollView style={{ backgroundColor: colors.background }}>
+				{
+					fields.map((field) => (
+						<CustomTextInput
+							key={field.stateKey}
+							onChangeText={(text) => this.onInputChange(text, field.stateKey)}
+							{...field}
+						/>
+					))
+				}
+				<View style={{ marginTop: 20 }}>
+					<PrimaryButton
+						label="Save"
+					    onPress={() => this.handleSubmit()}
+					/>
+				</View>
+			</KeyboardAwareScrollView>
 		);
 	}
 }
